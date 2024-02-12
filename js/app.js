dbef87e054993cfd7a78c7306b21bf871fea2309
vaisakhp98 <vaisakhp43@gmail.com>
2024-02-12 11:10:58 +0530

    Tablet display

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    let breakLine = document.getElementById("breakLine");
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth === 768) {
+        breakLine.remove();
+    }
+});
